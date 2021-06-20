c00b31f64a695bbc9fe8bdda4d594596470e7071
Welton Leite <av4t@hotmail.com>
2021-06-19 23:54:46 -0300

    ex00: OK | ex01: OK | ex02: OK | ex03: OK

---
+++ c06/ex03/ft_sort_params.c
@@ -0,0 +1,76 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return ((int)(unsigned char)s1[i] - (int)(unsigned char)s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
+
+void	ft_sort_params(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 0;
+	while (i < argc)
+	{
+		j = 1;
+		while (j < argc - 1)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			{
+				temp = argv[j];
+				argv[j] = argv[j + 1];
+				argv[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	ft_sort_params(argc, argv);
+	i = 1;
+	if (argc > 1)
+	{
+		while (i < argc)
+		{
+			ft_putstr(argv[i]);
+			ft_putstr("\n");
+			i++;
+		}
+	}
+	return (0);
+}
